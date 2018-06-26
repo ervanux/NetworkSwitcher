8fa9413cd21c17fc13136dab3a722a5143a13993
Erkan Ugurlu [Dijital Inovasyon Atolyesi] <Erkan.Ugurlu@ingbank.com.tr>
2018-06-26 15:26:07 +0300

    Initial Commit

---
+++ NetworkSwitcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NetworkSwitcher
+//
+//  Created by Erkan Ugurlu [Dijital Inovasyon Atolyesi] on 26.06.2018.
+//  Copyright © 2018 Ervanux. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NetworkSwitcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NetworkSwitcher
+//
+//  Created by Erkan Ugurlu [Dijital Inovasyon Atolyesi] on 26.06.2018.
+//  Copyright © 2018 Ervanux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
